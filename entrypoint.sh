ccc113f738842d090708ae52f9029316e42468a7
Peter Newman <peternewman@users.noreply.github.com>
2020-06-03 23:15:07 +0100

    Add: options for ignores and line length (#9)

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,19 @@ echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/flake8-matcher.json"
 # The weird piping is needed because we want to get the exitcode from flake8,
 # but there is no other good universal way of doing so.
 # e.g. PIPESTATUS and pipestatus only work in bash/zsh respectively.
-echo "Running flake8 on '${INPUT_PATH}' ..."
+echo "Running flake8 on '${INPUT_PATH}' with the following options..."
+command_args=""
+echo "Ignoring '${INPUT_IGNORE}'"
+if [ "x${INPUT_IGNORE}" != "x" ]; then 
+    command_args="${command_args} --ignore ${INPUT_IGNORE}"
+fi
+echo "Max line length '${INPUT_MAX_LINE_LENGTH}'"
+if [ "x${INPUT_MAX_LINE_LENGTH}" != "x" ]; then 
+    command_args="${command_args} --max-line-length ${INPUT_MAX_LINE_LENGTH}"
+fi
+echo "Resulting CLI options ${command_args}"
 exec 5>&1
-res=`{ { flake8 ${INPUT_PATH}; echo $? 1>&4; } | sed -r 's/: ([^W][0-9][0-9][0-9])/: error: \1/;s/: (W[0-9][0-9][0-9])/: warning: \1/' 1>&5; } 4>&1`
+res=`{ { flake8 ${command_args} ${INPUT_PATH}; echo $? 1>&4; } | sed -r 's/: ([^W][0-9][0-9][0-9])/: error: \1/;s/: (W[0-9][0-9][0-9])/: warning: \1/' 1>&5; } 4>&1`
 if [ "$res" = "0" ]; then
     echo "Flake8 found no problems"
 else
